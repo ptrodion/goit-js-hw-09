a6100b6d517a6975679d3e1ce8c79ed5a61c728c
ptrodion <rdnpetrenko@gmail.com>
2023-07-05 23:43:03 +0100

    add 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  startEl: document.querySelector('[data-start]'),
+  stopEl: document.querySelector('[data-stop]'),
+};
+
+refs.stopEl.disabled = true;
+let changeBackgroundColor = null;
+
+refs.startEl.addEventListener('click', onClickStart);
+refs.stopEl.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  refs.startEl.disabled = true;
+  refs.stopEl.disabled = false;
+  changeBackgroundColor = setInterval(changeBodyBackgroundColor, 1000);
+}
+
+function changeBodyBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onClickStop() {
+  clearInterval(changeBackgroundColor);
+  refs.stopEl.disabled = true;
+  refs.startEl.disabled = false;
+}
